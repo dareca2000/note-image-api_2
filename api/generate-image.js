c16608e9ca01adffbfd7a3ccbbad2778f4917ec6
dareca2000 <okadayutayoyaku@gmail.com>
2025-06-04 11:03:43 +0900

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -1,10 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import Jimp from 'jimp';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default async function handler(req, res) {
   const { date = '2025/06/04' } = req.query;
 
   try {
-    const image = await Jimp.read('https://note-image-api-2.vercel.app/base.png');
+    const imagePath = path.join(__dirname, '..', 'public', 'base.png');
+    const image = await Jimp.read(imagePath);
     const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
     image.print(font, 50, 400, `更新日: ${date}`);
 
