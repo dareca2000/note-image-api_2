2cf47fdb516cf15b775fd3f9bf40e923b77ef6c2
dareca2000 <okadayutayoyaku@gmail.com>
2025-06-04 11:00:17 +0900

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const { date = '2025/06/04' } = req.query;
 
   try {
-    const image = await Jimp.read('./public/base.png');
+    const image = await Jimp.read('https://note-image-api-2.vercel.app/base.png');
     const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
     image.print(font, 50, 400, `更新日: ${date}`);
 
